da4a3dde38f4951d303c5a96273ecb0beb214975
Yong Lee <nogy21@gmail.com>
2021-11-08 15:53:09 +0900

    초기 설정(MemberVO, MemberDAO)

--- kostagram-project/src/main/java/org/kostagram/model/MemberDAO.java
+++ kostagram-project/src/main/java/org/kostagram/model/MemberDAO.java
@@ -1,5 +1,30 @@
 package org.kostagram.model;
 
-public class MemberDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+public class MemberDAO {
+    private static MemberDAO instance = new MemberDAO();
+    private DataSource dataSource;
+    private MemberDAO() {
+        this.dataSource = DataSourceManager.getInstance().getDataSource();
+    }
+    public static MemberDAO getInstance() {
+        return instance;
+    }
+    public void closeAll(ResultSet rs, PreparedStatement pstmt, Connection con) throws SQLException {
+        if (rs != null)
+            rs.close();
+        closeAll(pstmt, con);
+    }
+    public void closeAll(PreparedStatement pstmt, Connection con) throws SQLException {
+        if (pstmt != null)
+            pstmt.close();
+        if (con != null)
+            con.close();
+    }
 }
--- kostagram-project/src/main/java/org/kostagram/model/MemberVO.java
+++ kostagram-project/src/main/java/org/kostagram/model/MemberVO.java
@@ -1,5 +1,79 @@
 package org.kostagram.model;
 
-public class MemberVO {
+import java.io.Serializable;
 
+public class MemberVO implements Serializable{
+    private static final long serialVersionUID = 894080600901728582L;
+    private String memberId;
+    private String password;
+    private String email;
+    private String name;
+    private String profileImg;
+    private String introduce;
+    private String registerDate;
+    public MemberVO() {
+        super();
+    }
+    public MemberVO(String memberId, String password, String email, String name, String profileImg, String introduce,
+            String registerDate) {
+        super();
+        this.memberId = memberId;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+        this.profileImg = profileImg;
+        this.introduce = introduce;
+        this.registerDate = registerDate;
+    }
+    public String getMemberId() {
+        return memberId;
+    }
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getProfileImg() {
+        return profileImg;
+    }
+    public void setProfileImg(String profileImg) {
+        this.profileImg = profileImg;
+    }
+    public String getIntroduce() {
+        return introduce;
+    }
+    public void setIntroduce(String introduce) {
+        this.introduce = introduce;
+    }
+    public String getRegisterDate() {
+        return registerDate;
+    }
+    public void setRegisterDate(String registerDate) {
+        this.registerDate = registerDate;
+    }
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+    @Override
+    public String toString() {
+        return "MemberVO [memberId=" + memberId + ", password=" + password + ", email=" + email + ", name=" + name
+                + ", profileImg=" + profileImg + ", introduce=" + introduce + ", registerDate=" + registerDate + "]";
+    }
+    
 }
