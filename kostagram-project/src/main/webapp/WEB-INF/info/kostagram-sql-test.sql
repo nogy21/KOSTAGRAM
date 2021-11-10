83b4487586e87ee8c260d9bd5bd18050011011b1
westkitekim <westkite418@gmail.com>
2021-11-10 11:51:27 +0900

    test.sql 수정

--- kostagram-project/src/main/webapp/WEB-INF/info/kostagram-sql-test.sql
+++ kostagram-project/src/main/webapp/WEB-INF/info/kostagram-sql-test.sql
@@ -1,6 +1,6 @@
-select * from kosta_member;
+select * from member;
 
-CREATE TABLE kosta_member (
+CREATE TABLE member (
     member_id VARCHAR2(100) PRIMARY KEY,
     password VARCHAR2(100) NOT NULL,
     email VARCHAR2(100) NOT NULL,
@@ -9,3 +9,16 @@ CREATE TABLE kosta_member (
     introduce CLOB,
     register_date DATE NOT NULL
 );
+
+CREATE TABLE post
+(
+    post_id NUMBER  PRIMARY KEY,
+    member_id VARCHAR2(100)  NOT NULL,
+    img VARCHAR2(500)  NOT NULL,
+    org_img VARCHAR2(500)  NOT NULL,
+    post_content CLOB,
+    post_date DATE  NOT NULL, 
+    constraint post_member_fk foreign key(member_id) references member(member_id)
+);
+
+CREATE sequence post_seq;
